d3f313bfbf5c43ea41d515f84d262e5cb3a5c84f
Nomen-Heroum <th.douwes@gmail.com>
2020-12-23 19:15:04 +0100

    initial commit

---
+++ 2020/day22.py
@@ -0,0 +1,44 @@
+import src
+from copy import deepcopy
+
+PLAYERS = src.read(22, '\n\n')
+
+
+def play_combat(players, recursive=False):
+    win = 0
+    cache = [deepcopy(players)]
+    while all(players):
+        cards = [p.pop(0) for p in players]
+        if recursive and all(len(players[i]) >= cards[i] for i in range(2)):
+            _, win = play_combat([players[i][:cards[i]] for i in range(2)], recursive=True)
+        else:
+            win = max(range(2), key=lambda x: cards[x])
+        players[win] += [cards[win], cards[(win + 1) % 2]]
+        if players in cache:
+            win = 0
+            break
+        else:
+            cache.append(deepcopy(players))
+
+    return players, win
+
+
+def calculate_score(players, win):
+    winner = players[win]
+    return sum(card * (i+1) for i, card in enumerate(winner[::-1]))
+
+
+def main(players=PLAYERS):
+    src.one()
+    players = [[int(s) for s in players[i].splitlines()[1:]] for i in range(2)]
+    ans1 = calculate_score(*play_combat(deepcopy(players)))
+    print(f"The final score is {ans1}.")
+
+    src.two()
+    ans2 = calculate_score(*play_combat(deepcopy(players), recursive=True))
+    print(f"The winner of Recursive Combat had {ans2} points.")
+    src.copy(ans2)
+
+
+if __name__ == '__main__':
+    main()
