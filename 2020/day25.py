c6b903249fb442b9bb04fead449b1af270c4ad07
Nomen-Heroum <th.douwes@gmail.com>
2020-12-25 18:00:01 +0100

    initial commit

---
+++ 2020/day25.py
@@ -0,0 +1,36 @@
+import src
+
+PUBLIC_KEY_1 = 6930903
+PUBLIC_KEY_2 = 19716708
+MODULUS = 20201227
+
+
+def find_encryption_key(public_key_1: int, public_key_2: int, modulus: int):
+    num = 1
+    loop_sizes = []
+    for i in range(1, modulus+1):
+        num = num*7 % modulus
+        if num == public_key_1 or num == public_key_2:
+            print(f"Found loop size: {i}")
+            loop_sizes.append(i)
+            if len(loop_sizes) == 2:
+                break
+        if (i+1) % 10_000 == 0:
+            print(f"{i+1} out of {modulus} combinations tried.\r", end='')
+
+    print("Calculating encryption key...")
+    loop_prod = loop_sizes[0] * loop_sizes[1]
+    loop_mod, remainder = divmod(loop_prod, modulus)
+    return 7**sum(divmod(loop_mod + remainder, modulus)) % modulus
+
+
+def main(public_key_1=PUBLIC_KEY_1, public_key_2=PUBLIC_KEY_2, modulus=MODULUS):
+    src.one()
+    ans1 = find_encryption_key(public_key_1, public_key_2, modulus)
+    print(f"The encryption key is {ans1}.")
+
+    src.copy(ans1)
+
+
+if __name__ == '__main__':
+    main()
