1db5cf388d54fb5c7c107038bbdd4ad0fda2a63a
Nomen-Heroum <th.douwes@gmail.com>
2020-12-28 19:01:11 +0100

    initial commit

---
+++ 2015/day24.py
@@ -0,0 +1,31 @@
+import src
+import itertools
+import math
+
+WEIGHTS = [int(s) for s in src.read(24)]
+
+
+def configure(weights: list, groups=3):
+    assert sum(weights) % groups == 0, f"Total weight is not a multiple of {groups}."
+    target = sum(weights) // groups
+    for size in itertools.count(1):
+        qe_list = [math.prod(c) for c in itertools.combinations(weights, size) if sum(c) == target]
+        if qe_list:
+            return min(qe_list)
+
+
+def main(weights=None):
+    weights = weights or WEIGHTS
+
+    src.one()
+    ans1 = configure(weights)
+    print(f"The best configuration has a QE of {ans1}.")
+
+    src.two()
+    ans2 = configure(weights, groups=4)
+    print(f"The best QE is now {ans2}.")
+    src.copy(ans2)
+
+
+if __name__ == '__main__':
+    main()
