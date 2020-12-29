839b88508a5a93014e09feecf7d8358e306cf1d1
Nomen-Heroum <th.douwes@gmail.com>
2020-12-29 17:44:01 +0100

    initial commit

---
+++ 2016/day8.py
@@ -0,0 +1,34 @@
+import src
+import numpy as np
+import re
+import matplotlib.pyplot as plt
+
+INSTRUCTIONS = src.read(8)
+SCREEN = np.zeros((6, 50), dtype=int)
+
+
+def follow(instructions, screen):
+    for instr in instructions:
+        a, b = (int(s) for s in re.findall(r'\d+', instr))
+        if 'rect' in instr:
+            screen[:b, :a] = 1
+        elif 'row' in instr:
+            screen[a] = np.concatenate((screen[a, -b:], screen[a, :-b]))
+        else:
+            screen[:, a] = np.concatenate((screen[-b:, a], screen[:-b, a]))
+    plt.imshow(screen)
+    return screen.sum()
+
+
+def main(instructions=INSTRUCTIONS, screen=SCREEN):
+    src.one()
+    ans1 = follow(instructions, screen)
+    print(f"{ans1} lights are on after following the instructions.")
+    src.copy(ans1)
+
+    src.two()
+    print(f"Plotted the code on the display.")
+
+
+if __name__ == '__main__':
+    main()
