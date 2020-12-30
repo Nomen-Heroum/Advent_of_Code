48111a1e835bcd298d838d080c22c88c8730ed74
Nomen-Heroum <th.douwes@gmail.com>
2020-12-30 13:01:25 +0100

    initial commit

---
+++ 2016/day9.py
@@ -0,0 +1,34 @@
+import src
+import re
+
+FILE = src.read(9)[0]
+
+
+def decompress(file: str, version=1):
+    mo = re.search(r'\((\d+)x(\d+)\)', file)
+    if mo:
+        chars, reps = int(mo[1]), int(mo[2])
+        a = mo.start()
+        b = mo.end()
+        c = b + chars
+        if version == 1:
+            return a + chars * reps + decompress(file[c:])
+        else:
+            return a + decompress(file[b:c], 2) * reps + decompress(file[c:], 2)
+    else:
+        return len(file)
+
+
+def main(file=FILE):
+    src.one()
+    ans1 = decompress(file)
+    print(f"The decompressed file length is {ans1}.")
+
+    src.two()
+    ans2 = decompress(file, version=2)
+    print(f"The fully decompressed length is {ans2}.")
+    src.copy(ans2)
+
+
+if __name__ == '__main__':
+    main()
