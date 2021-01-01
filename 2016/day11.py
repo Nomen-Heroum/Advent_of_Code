7b074706dbc5f3638ee39e9826cc9d51b174a078
Nomen-Heroum <th.douwes@gmail.com>
2021-01-01 18:48:32 +0100

    Moved a_star to src, cleaned up src.neighbours()

--- 2016/day11.py
+++ 2016/day11.py
@@ -1,5 +1,4 @@
 import src
-import heapq
 
 # Nodes contain all generator floors, then all respective microchip floors, then the current floor.
 # Floors are numbered as their distance from the 4th floor, for nicer heuristic calculation.
@@ -51,80 +50,13 @@ def heuristic(node, _target):
     return 2 * sum(node[0]) - 3 * node[1]
 
 
-def a_star(start, target, h, neighbours_costs, admissible=True):
-    """Generic A* pathfinding algorithm. Does not return a path, only the discovered path length.
-
-    A binary heap (heapq) is used for the priority queue. f(n) = g(n) + h(n) is prioritised as always.
-    In case of a tie, the node with the lowest heuristic h(n) is prioritised. Further ties are handled
-    in reverse insertion order (Last In, First Out).
-
-    Different algorithms are used for admissible and non-admissible heuristics; the admissible case is
-    faster, but it breaks for improper heuristics.
-
-    Nodes are stored in sets, so they must be hashable.
-
-    Args:
-        start: The starting node.
-        target: The target node.
-        h: A function that returns the heuristic h(n) of a given node. Takes two arguments: (node, target)
-        neighbours_costs: A single-argument function that takes the current node, and yields tuples containing
-            each neighbouring node and its distance from the current node: (neighbour, cost)
-        admissible (optional): True if the heuristic is admissible (default), False otherwise. Must be set to
-            False for non-admissible heuristics to prevent breakage.
-
-    Returns:
-        The length of the discovered path.
-    """
-    guess = h(start, target)  # Best guess for the shortest path length
-    entry_id = 0
-
-    # Priority queue: f(n) is prioritised, with ties broken first by h(n), then by last inserted.
-    queue = [(guess, guess, entry_id, start, 0)]  # (f(n), h(n), entry, node, g(n))
-
-    if admissible:  # Faster, more breakable algorithm
-        visited = {start}
-
-        while queue:
-            _f, _h, _id, node, g = heapq.heappop(queue)
-            for neigh, cost in neighbours_costs(node):
-                if neigh == target:  # Return straight away when we touch the target node
-                    return g + cost
-                if neigh not in visited:
-                    entry_id -= 1  # Negative to ensure LIFO
-                    g_n = g + cost
-                    h_n = h(neigh, target)
-                    f_n = g_n + h_n  # Total path cost
-                    heapq.heappush(queue, (f_n, h_n, entry_id, neigh, g_n))
-                    visited.add(neigh)  # We mark the neighbours as visited to prevent duplicates
-
-    else:  # Slower, more resilient algorithm
-        visited = set()
-
-        while queue:
-            _f, _h, _id, node, g = heapq.heappop(queue)
-            if node == target:  # Only return once the target node is current
-                return g
-            if node not in visited:
-                visited.add(node)  # Without an admissible h(n) we have to mark the current node as visited
-                for neigh, cost in neighbours_costs(node):
-                    if neigh not in visited:
-                        entry_id -= 1  # Negative to ensure LIFO
-                        g_n = g + cost
-                        h_n = h(neigh, target)
-                        f_n = g_n + h_n  # Total path cost
-                        heapq.heappush(queue, (f_n, h_n, entry_id, neigh, g_n))
-
-    # If every possible node has been visited
-    raise EOFError("No path to the target could be found.")
-
-
 def main():
     src.one()
-    ans1 = a_star(START, TARGET, heuristic, neighbours)
+    ans1 = src.a_star(START, TARGET, heuristic, neighbours)
     print(f"You need at least {ans1} steps.")
 
     src.two()
-    ans2 = a_star(START_2, TARGET_2, heuristic, neighbours)
+    ans2 = src.a_star(START_2, TARGET_2, heuristic, neighbours)
     print(f"With the extra objects, the minimum is {ans2} steps.")
     src.copy(ans2)
 
--- src.py
+++ src.py
@@ -1,7 +1,7 @@
 """
 This file contains general use functions for all exercises.
 """
-
+import heapq
 import os
 import re
 
@@ -31,17 +31,80 @@ def two():
 
 
 def neighbours(x: int, y: int, grid: np.ndarray):
-    yield grid[x, y - 1]
-    yield grid[x, y + 1]
-    yield grid[x - 1, y]
-    yield grid[x + 1, y]
-    yield grid[x - 1, y - 1]
-    yield grid[x - 1, y + 1]
-    yield grid[x + 1, y - 1]
-    yield grid[x + 1, y + 1]
+    for dx in (-1, 0, 1):
+        for dy in (-1, 0, 1):
+            if (dx, dy) != (0, 0):
+                yield grid[x + dx, y + dy]
 
 
 def repeat(f, x, n: int):
     for i in range(n):
         x = f(x)
     return x
+
+
+def a_star(start, target, h, neighbours_costs, admissible=True):
+    """Generic A* pathfinding algorithm. Does not return a path, only the discovered path length.
+
+    A binary heap (heapq) is used for the priority queue. f(n) = g(n) + h(n) is prioritised as always.
+    In case of a tie, the node with the lowest heuristic h(n) is prioritised. Further ties are handled
+    in reverse insertion order (Last In, First Out).
+
+    Different algorithms are used for admissible and non-admissible heuristics; the admissible case is
+    faster, but it breaks for improper heuristics.
+
+    Nodes are stored in sets, so they must be hashable.
+
+    Args:
+        start: The starting node.
+        target: The target node.
+        h: A function that returns the heuristic h(n) of a given node. Takes two arguments: (node, target)
+        neighbours_costs: A single-argument function that takes the current node, and yields tuples containing
+            each neighbouring node and its distance from the current node: (neighbour, cost)
+        admissible (optional): True if the heuristic is admissible (default), False otherwise. Must be set to
+            False for non-admissible heuristics to prevent breakage.
+
+    Returns:
+        The length of the discovered path.
+    """
+    guess = h(start, target)  # Best guess for the shortest path length
+    entry_id = 0
+
+    # Priority queue: f(n) is prioritised, with ties broken first by h(n), then by last inserted.
+    queue = [(guess, guess, entry_id, start, 0)]  # (f(n), h(n), entry, node, g(n))
+
+    if admissible:  # Faster, more breakable algorithm
+        visited = {start}
+
+        while queue:
+            _f, _h, _id, node, g = heapq.heappop(queue)
+            for neigh, cost in neighbours_costs(node):
+                if neigh == target:  # Return straight away when we touch the target node
+                    return g + cost
+                if neigh not in visited:
+                    entry_id -= 1  # Negative to ensure LIFO
+                    g_n = g + cost
+                    h_n = h(neigh, target)
+                    f_n = g_n + h_n  # Total path cost
+                    heapq.heappush(queue, (f_n, h_n, entry_id, neigh, g_n))
+                    visited.add(neigh)  # We mark the neighbours as visited to prevent duplicates
+
+    else:  # Slower, more resilient algorithm
+        visited = set()
+
+        while queue:
+            _f, _h, _id, node, g = heapq.heappop(queue)
+            if node == target:  # Only return once the target node is current
+                return g
+            if node not in visited:
+                visited.add(node)  # Without an admissible h(n) we have to mark the current node as visited
+                for neigh, cost in neighbours_costs(node):
+                    if neigh not in visited:
+                        entry_id -= 1  # Negative to ensure LIFO
+                        g_n = g + cost
+                        h_n = h(neigh, target)
+                        f_n = g_n + h_n  # Total path cost
+                        heapq.heappush(queue, (f_n, h_n, entry_id, neigh, g_n))
+
+    # If every possible node has been visited
+    raise EOFError("No path to the target could be found.")
