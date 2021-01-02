8de83f445ffa3e44e7fcaa4716301b960a09eb40
Nomen-Heroum <th.douwes@gmail.com>
2021-01-02 12:54:04 +0100

    initial commit

---
+++ 2016/day14.py
@@ -0,0 +1,51 @@
+import src
+import itertools
+import hashlib
+import re
+
+SALT = 'ihaygndm'
+
+
+def find_otp(salt, version=1):
+    lowest = 0
+    potential_keys = {}
+    key_indices = []
+
+    def encode(string):
+        return hashlib.md5(string.encode('utf-8')).hexdigest()
+
+    for i in itertools.count():
+        hash_input = salt + str(i)
+        hexa = encode(hash_input) if version == 1 else src.repeat(encode, hash_input, 2017)
+        mo = re.search(r'(.)\1\1', hexa)
+        if mo:
+            if not potential_keys:
+                lowest = i
+            potential_keys[i] = [mo[1], hexa, False]
+
+            for index, (char, _hex, _bool) in potential_keys.items():
+                if re.search(char*5, hexa) and index != i:
+                    potential_keys[index][-1] = True
+
+        if potential_keys and i == lowest + 1000:
+            if potential_keys[lowest][-1]:
+                key_indices.append(lowest)
+                if len(key_indices) == 64:
+                    return lowest
+            del potential_keys[lowest]
+            lowest = min(potential_keys) if potential_keys else 0
+
+
+def main(salt=SALT):
+    src.one()
+    ans1 = find_otp(salt)
+    print(f"Index {ans1} gives the 64th key.")
+
+    src.two()
+    ans2 = find_otp(salt, version=2)
+    print(f"With key stretching, this index is {ans2}.")
+    src.copy(ans2)
+
+
+if __name__ == '__main__':
+    main()
