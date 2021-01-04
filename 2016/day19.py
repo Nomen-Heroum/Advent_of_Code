dec5fbe8658312ae6bae65902c996760495690f5
Nomen-Heroum <th.douwes@gmail.com>
2021-01-04 13:22:07 +0100

    initial commit

---
+++ 2016/day19.py
@@ -0,0 +1,40 @@
+import src
+from collections import deque
+
+ELVES = 3017957
+
+
+def lucky_elf(elves, version=1):
+    queue = deque(range(1, elves + 1))
+    queue.reverse()
+
+    if version == 1:
+        while len(queue) > 1:
+            queue.rotate()
+            queue.pop()
+    else:
+        half, odd = divmod(elves, 2)
+        queue.rotate(half)
+        if not odd:
+            queue.pop()
+        while len(queue) > 1:
+            queue.pop()
+            queue.rotate()
+            queue.pop()
+
+    return queue[0]
+
+
+def main(elves=ELVES):
+    src.one()
+    ans1 = lucky_elf(elves)
+    print(f"The lucky elf is Elf {ans1}.")
+
+    src.two()
+    ans2 = lucky_elf(elves, 2)
+    print(f"The lucky elf is now Elf {ans2}.")
+    src.copy(ans2)
+
+
+if __name__ == '__main__':
+    main()
