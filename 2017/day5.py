0df471bc89b9e484625be96129749f4345ecd403
Nomen-Heroum <th.douwes@gmail.com>
2021-01-05 17:24:35 +0100

    initial commit

---
+++ 2017/day5.py
@@ -0,0 +1,34 @@
+import src
+from copy import copy
+
+JUMPS = [int(n) for n in src.read(5)]
+
+
+def count_steps(jumps, version=1):
+    index = 0
+    steps = 0
+    while True:
+        try:
+            jump = jumps[index]
+            jumps[index] += 1 if version == 1 or jump < 3 else -1
+            index += jump
+            steps += 1
+        except IndexError:
+            return steps
+
+
+def main(jumps=None):
+    jumps = jumps or JUMPS
+
+    print("Part One:")
+    ans1 = count_steps(copy(jumps))  # 381680
+    print(f"It takes {ans1} jumps to reach the exit.")
+
+    print("\nPart Two:")
+    ans2 = count_steps(copy(jumps), version=2)  # 29717847
+    print(f"It now takes {ans2} steps.")
+    src.copy(ans2)
+
+
+if __name__ == '__main__':
+    main()
