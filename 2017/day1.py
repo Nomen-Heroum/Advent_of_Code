766d89351f66227a772dc7d41ce4c93b447ca7fe
Nomen-Heroum <th.douwes@gmail.com>
2021-01-05 16:32:44 +0100

    initial commit

---
+++ 2017/day1.py
@@ -0,0 +1,24 @@
+import src
+
+STRING = src.read(1)[0]
+
+
+def solve_captcha(string, version=1):
+    n = len(string)
+    step = 1 if version == 1 else n//2
+    return sum(int(char) for i, char in enumerate(string) if string[(i+step) % n] == char)
+
+
+def main(string=STRING):
+    print("Part One:")
+    ans1 = solve_captcha(string)  # 1158
+    print(f"The solution to my captcha is {ans1}.")
+
+    print("\nPart Two:")
+    ans2 = solve_captcha(string, version=2)  # 1132
+    print(f"The solution to the second captcha is {ans2}.")
+    src.copy(ans2)
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2017/day2.py
@@ -0,0 +1,36 @@
+import src
+
+NUMBERS = [[int(n) for n in s.split()] for s in src.read(2)]
+
+
+def checksum(numbers):
+    return sum(max(row) - min(row) for row in numbers)
+
+
+def sum_divisions(numbers):
+    def find_quotient(row):
+        for i, num1 in enumerate(row):
+            candidates = (n for n in row[:i] + row[i+1:] if n <= num1)
+            for num2 in candidates:
+                quotient, remainder = divmod(num1, num2)
+                if remainder == 0:
+                    return quotient
+
+    return sum(find_quotient(r) for r in numbers)
+
+
+def main(numbers=None):
+    numbers = numbers or NUMBERS
+
+    print("Part One:")
+    ans1 = checksum(numbers)  # 36766
+    print(f"The checksum of this spreadsheet is {ans1}.")
+
+    print("\nPart Two:")
+    ans2 = sum_divisions(numbers)  # 16
+    print(f"The sum of each even division is {ans2}.")
+    src.copy(ans2)
+
+
+if __name__ == '__main__':
+    main()
