67576a99c3e042623923fd05a695c6b2a9d39932
Nomen-Heroum <th.douwes@gmail.com>
2021-01-11 22:17:25 +0100

    initial commit

---
+++ 2018/day1.py
@@ -0,0 +1,29 @@
+import src
+
+DRIFTS = [int(n) for n in src.read()]
+
+
+def first_repetition(drifts):
+    frequency = 0
+    seen = {0}
+    while True:
+        for n in drifts:
+            frequency += n
+            if frequency in seen:
+                return frequency
+            seen.add(frequency)
+
+
+def main():
+    print("Part One:")
+    ans1 = sum(DRIFTS)  # 500
+    print(f"The resulting frequency is {ans1}.")
+
+    print("\nPart Two:")
+    ans2 = first_repetition(DRIFTS)  # 709
+    print(f"The first frequency reached twice is {ans2}.")
+    src.copy(ans2)
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2018/day2.py
@@ -0,0 +1,37 @@
+import src
+from collections import Counter
+
+BOX_IDS = src.read()
+
+
+def checksum(box_ids):
+    two_count = 0
+    three_count = 0
+    for box_id in box_ids:
+        letter_counts = Counter(box_id).values()
+        two_count += 2 in letter_counts
+        three_count += 3 in letter_counts
+    return two_count * three_count
+
+
+def common_letters(box_ids):
+    id_length = len(box_ids[0])
+    for i, id1 in enumerate(box_ids):
+        for id2 in box_ids[i+1:]:
+            if sum(id1[i] != id2[i] for i in range(id_length)) == 1:
+                return ''.join(c for i, c in enumerate(id1) if id2[i] == c)
+
+
+def main(box_ids=BOX_IDS):
+    print("Part One:")
+    ans1 = checksum(box_ids)  # 5681
+    print(f"The checksum is {ans1}.")
+
+    print("\nPart Two:")
+    ans2 = common_letters(box_ids)  # uqyoeizfvmbistpkgnocjtwld
+    print(f"The common letters between the two correct IDs are {ans2}.")
+    src.copy(ans2)
+
+
+if __name__ == '__main__':
+    main()
