7603fa564676a1946b00a8b0334cbceb623ccde2
Nomen-Heroum <th.douwes@gmail.com>
2021-01-20 22:07:41 +0100

    Initial commit

---
+++ 2019/day6.py
@@ -0,0 +1,50 @@
+import src
+from collections import defaultdict
+
+STRINGS = src.read()
+
+
+def count_orbits(strings):
+    orbits = defaultdict(set)
+    for s in strings:
+        primary, secondary = s.split(')')
+        orbits[primary].add(secondary)
+
+    total = 0
+    level = 1
+    current = {'COM'}
+    while current:
+        new = set()
+        for body in current:
+            new |= orbits[body]
+            total += level * len(orbits[body])
+        current = new
+        level += 1
+
+    return total, orbits
+
+
+def neighbours(body, orbits, orbiting):
+    if body in orbits:
+        yield from orbits[body]
+    if body in orbiting:
+        yield orbiting[body]
+
+
+def main(strings=STRINGS):
+    print("Part One:")
+    ans1, orbits = count_orbits(strings)  # 247089
+    print(f"The total number of direct and indirect orbits is {ans1}.")
+
+    print("\nPart Two:")
+    orbiting = {}
+    for s in strings:
+        primary, secondary = s.split(')')
+        orbiting[secondary] = primary
+    ans2 = src.a_star('YOU', 'SAN', lambda _n, _t: 0, neighbours, orbits=orbits, orbiting=orbiting) - 2
+    print(f"It takes {ans2} orbital transfers to get to Santa.")  # 442
+    src.clip(ans2)
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2019/day7.py
@@ -0,0 +1,35 @@
+import src
+from itertools import permutations
+
+INTCODE = src.read(',', ints=True)
+
+
+def try_phases(intcode, phases):
+    max_signal = 0
+    for sequence in permutations(phases):
+        cpus = [src.IntCodeCPU(intcode, phase) for phase in sequence]
+        signal = 0
+        amp = 0
+        while cpus[amp].running:
+            cpus[amp].input.append(signal)
+            signal = cpus[amp].execute()[-1]
+            amp += 1
+            amp %= 5
+        max_signal = max(max_signal, signal)
+    return max_signal
+
+
+def main(intcode=INTCODE):
+    cpu = src.IntCodeCPU()
+    print("Part One:")
+    ans1 = try_phases(intcode, range(5))
+    print(f"The highest possible signal is {ans1}.")  # 75228
+
+    print("\nPart Two:")
+    ans2 = try_phases(intcode, range(5, 10))  # 79846026
+    print(f"The highest signal with feedback is {ans2}.")
+    src.clip(ans2)
+
+
+if __name__ == '__main__':
+    main()
