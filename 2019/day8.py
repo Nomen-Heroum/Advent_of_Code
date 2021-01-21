dab64c4afd0ff1815814e87e3330e5309cc06c88
Nomen-Heroum <th.douwes@gmail.com>
2021-01-21 18:09:17 +0100

    Initial commit

---
+++ 2019/day8.py
@@ -0,0 +1,43 @@
+import src
+import numpy as np
+import matplotlib.pyplot as plt
+
+DIGITS = [int(n) for n in src.read()[0]]
+
+
+def find_layer(digits, width=25, height=6):
+    n = width * height
+    min_zeros = n
+    best_layer = None
+    for layer in (digits[i:i+n] for i in range(0, len(digits), n)):
+        zeros = layer.count(0)
+        if zeros < min_zeros:
+            min_zeros = zeros
+            best_layer = layer
+    return best_layer.count(1) * best_layer.count(2)
+
+
+def decode_image(digits, width=25, height=6):
+    layers = np.reshape(digits, (-1, height, width))
+    image = np.full_like(layers[0], 2)
+    for layer in layers:
+        undetermined = image == 2
+        image[undetermined] = layer[undetermined]
+    plt.imshow(image)
+
+
+def main(digits=None):
+    digits = digits or DIGITS
+
+    print("Part One:")
+    ans1 = find_layer(digits)  # 2975
+    print(f"The checksum of the layer with least zeros is {ans1}.")
+    src.clip(ans1)
+
+    print("\nPart Two:")
+    decode_image(digits)  # EHRUE
+    print("Plotted the image.")
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2019/day9.py
@@ -0,0 +1,21 @@
+import src
+
+INTCODE = src.read(',', ints=True)
+
+
+def main(intcode=INTCODE):
+    cpu = src.IntCodeCPU()
+    print("Part One:")
+    output = cpu.execute(intcode, 1)
+    assert len(output) == 1, f"Some opcodes have malfunctioned: {output[:-1]}"
+    ans1, = output  # 2457252183
+    print(f"The BOOST keycode produced is {ans1}.")
+
+    print("\nPart Two:")
+    ans2, = cpu.execute(intcode, 2)  # 70634
+    print(f"The coordinates of the distress signal are {ans2}.")
+    src.clip(ans2)
+
+
+if __name__ == '__main__':
+    main()
